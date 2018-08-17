a37723315f0ce031a2067538814e8e9d7e414d3f
Administrador <Administrador@AUTVMLRY00010>
2018-08-16 22:52:29 -0300

    Inclusao de funcao para configuração de parametros

--- AUTDataFlow.java
+++ AUTDataFlow.java
@@ -3,6 +3,8 @@
  */
 package br.lry.dataflow;
 
+import br.lry.functions.AUTProjectsFunctions;
+
 /**
  * 
  * Gerenciamento do fluxo de dados na automação
@@ -13,4 +15,36 @@ package br.lry.dataflow;
  */
 public class AUTDataFlow {
 
+	
+	
+	
+	/**
+	 * 
+	 * Recupera um conjunto de parametros padrão para todos os sistemas
+	 * 
+	 * @return java.util.HashMap(String,String) - Matriz de dados do projeto
+	 * 
+	 * 
+	 */
+	public java.util.HashMap<String, String> autGetParameter(){
+		
+		java.util.HashMap<String,String> parametersOut = new java.util.HashMap<String,String>();
+		parametersOut.put("AUT_USER", "51028487");
+		parametersOut.put("AUT_PASSWORD","1234");	
+		
+		
+		return parametersOut;
+	}
+	
+	
+	
+	/**
+	 * 
+	 * Construtor padrão da classe
+	 * 
+	 */
+	public AUTDataFlow()
+	{
+		
+	}	
 }
