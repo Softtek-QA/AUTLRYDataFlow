01b7432037ec78d67b8bab93cefb02bcf53489f1
Administrador <Administrador@AUTVMLRY00001>
2018-09-11 18:42:41 -0300

    Inclusão do arquivo de dados - SPRINT 1

--- AUTDataFlow.java
+++ AUTDataFlow.java
@@ -197,7 +197,7 @@ public class AUTDataFlow {
 			vaDataGeracaoPedidos001.get(1).put("AUT_USUARIO_LOJA", "5500368019793");
 			vaDataGeracaoPedidos001.get(1).put("AUT_SENHA", "1234");
 			vaDataGeracaoPedidos001.get(1).put("AUT_QUANTIDADE_ITEM", "1");
-			vaDataGeracaoPedidos001.get(1).put("AUT_CODIGO_ITEM", "89368790");	// "89102006");
+			vaDataGeracaoPedidos001.get(1).put("AUT_CODIGO_ITEM", "89102006");	// "89296193"|"89368790");
 			vaDataGeracaoPedidos001.get(1).put("AUT_CPF_CLIENTE_NOVO", AUTProjectsFunctions.gerarCPF());
 			vaDataGeracaoPedidos001.get(1).put("AUT_CPF_CLIENTE_CADASTRADO", "78651738811");
 			vaDataGeracaoPedidos001.get(1).put("AUT_PASSAPORTE", "78651738811");
